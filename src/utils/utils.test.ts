68ed3ad888debed177d925864fec39a562aa952f
Jesse Wicks <33884732+jwicks31@users.noreply.github.com>
2018-08-14 10:06:46 -0500

    tests(unit-test): adds unit test for StringifySecure <subject> (#71)

---
+++ src/utils/utils.test.ts
@@ -0,0 +1,27 @@
+import { describe } from 'riteway'
+
+import { StringifySecure } from './utils'
+
+describe('StringifySecure', async (should: any) => {
+  const { assert } = should()
+  assert({
+    given: 'empty object',
+    should: 'return empty object in string',
+    actual: StringifySecure({}),
+    expected: '{}',
+  })
+
+  assert({
+    given: 'object',
+    should: 'return object as a string',
+    actual: StringifySecure({ key: 'value', key2: 'value2' }),
+    expected: '{"key":"value","key2":"value2"}',
+  })
+
+  assert({
+    given: 'object with undefined values',
+    should: 'return empty object as a string',
+    actual: StringifySecure({ key: undefined, key2: undefined }),
+    expected: '{}',
+  })
+})
